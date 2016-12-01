a73ac855c0cba55ecabdf005d7a5ef52ada50e43
Thomas Graves <thomaslogangraves@gmail.com>
2016-12-01 11:53:19 -0500

    framework for react component.

---
+++ src/components/dashboard.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { observer } from "mobx-react"
+
+
+@observer
+export default class TodoList extends React.Component {
+
+
+  render() {
+    return
+    <div>
+    </div>
+  }
+}
